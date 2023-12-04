0f2cc3398659dc04440ae087eef55446e125865f
SaadSnype <211009@students.au.edu.pk>
2023-12-04 14:43:23 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using CarRental;
+using CarRentalCommon.Interfaces;
+using CarRentalData.Classes;
+using CarRentalLogic.Classes;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddSingleton<IData, CollectionData>();
+builder.Services.AddSingleton<Services>();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
